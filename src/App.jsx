4d7bf7a33534a2761c8d72ec8827e168ea741b1c
Baibhav Kumar <baibhav.kr73@gmail.com>
2024-01-29 17:45:01 +0530

    project setup, tailwind, prettier and proxy in vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <h1
+      className="bg-orange-500 font-semibold p-5 text-center text-3xl
+  "
+    >
+      This is my e-commerce project for FEE
+    </h1>
+  );
+}
+
+export default App;
